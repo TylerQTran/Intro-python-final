c533d0522828801e4ca3261366df9ffe1e710706
Naeem Mannan <nmannan97@gmail.com>
2020-06-04 00:12:45 -0700

    Adding data

--- AbornSchool_IntroFinal.py
+++ AbornSchool_IntroFinal.py
@@ -1,9 +1,22 @@
 import matplotlib.pyplot as plt
 import math
+import csv
 
-x = []
+x_csv = []
+y_csv = []
+x = [] 
 y = []
 
-for i in range(1000):
-    nothing = 0
+for i in range(1001):
+    x_csv.append(str((2*math.pi*i)/1000))
+    y_csv.append(str(math.sin((2*math.pi*i)/1000)))
+    x.append(((2*math.pi*i)/1000))
+    y.append((math.sin((2*math.pi*i)/1000)))
 
+data = open('C:/Users/balto/OneDrive/Desktop/AbornSchool/data.csv','w')
+data2 = csv.writer(data)
+data2.writerow(x_csv)
+data2.writerow(y_csv)
+
+plt.scatter(x, y)
+plt.show()
