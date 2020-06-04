ef5472ad2a23b21ebc7b1ffce7d37b76c0600a25
Naeem Mannan <nmannan97@gmail.com>
2020-06-04 00:47:43 -0700

    Adding x and y data

--- AbornSchool_IntroFinal.py
+++ AbornSchool_IntroFinal.py
@@ -13,10 +13,19 @@ for i in range(1001):
     x.append(((2*math.pi*i)/1000))
     y.append((math.sin((2*math.pi*i)/1000)))
 
-data = open('C:/Users/balto/OneDrive/Desktop/AbornSchool/data.csv','w')
-data2 = csv.writer(data)
-data2.writerow(x_csv)
-data2.writerow(y_csv)
+data_x = open('C:/Users/balto/OneDrive/Desktop/AbornSchool/dataX.csv','w')
+data_y = open('C:/Users/balto/OneDrive/Desktop/AbornSchool/dataY.csv','w')
 
-plt.scatter(x, y)
-plt.show()
+data2 = csv.writer(data_x)
+data2.writerow({'X values of sin'})
+for i in range(len(x_csv)):
+    data2.writerow({x_csv[i]})
+
+data3 = csv.writer(data_y)
+data3.writerow({'Y values of sin'})
+for i in range(len(y_csv)):
+    data3.writerow({y_csv[i]})
+    #data2.writerow(y_csv[i])
+
+#plt.scatter(x, y)
+#plt.show()
