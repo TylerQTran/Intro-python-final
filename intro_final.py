9e29732a981a281c775c3277b72df676d1bc82a4
TylerQTran <67287706+TylerQTran@users.noreply.github.com>
2020-07-02 10:46:18 -0700

    intro python final

---
+++ intro_final.py
@@ -0,0 +1,11 @@
+import csv
+import matplotli
+
+dataX = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataX.csv')
+dataY = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataY.csv')
+
+csv.reader(dataX)
+csv.reader(dataY)
+
+open(dataY)
+open(dataX)
