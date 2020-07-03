2569612bc0c5b84518c2346b9fc99cd268801974
TylerQTran <67287706+TylerQTran@users.noreply.github.com>
2020-07-03 11:35:30 -0700

    final

--- intro_final.py
+++ intro_final.py
@@ -1,10 +1,21 @@
 import csv
-dataX = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataX.csv')
-dataY = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataY.csv')
+dataX = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataX.csv','r')
+dataY = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataY.csv','r')
 dataX = csv.reader(dataX)
 dataY = csv.reader(dataY)
 X = []
+Y = []
 for rows in dataX:
-    if len(float(rows[0])>0):
-         X.append(float(rows[0]))
+    if(len(rows)>0):
+        if "X" not in rows[0]:
+            X.append(float(rows[0]))
+for rows in dataY:
+    if(len(rows)>0):
+        if "Y" not in rows[0]:
+            Y.append(float(rows[0]))
 print(X)
+print(Y)
+sumofarea = 0
+for i in range len(Y):
+    ((X[1] - X[0])*Y) + sumofarea
+print(sumofarea)
