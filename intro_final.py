212818ee5d8a9ca9c66cd3c746ddea9616e4e4d7
TylerQTran <67287706+TylerQTran@users.noreply.github.com>
2020-07-03 09:06:02 -0700

    Update intro_final.py

--- intro_final.py
+++ intro_final.py
@@ -1,12 +1,13 @@
 import csv
-X = []
-
-dx = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataX.csv')
+dataX = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataX.csv')
 dataY = open('C:/Users/tyler/OneDrive/Documents/python/Intro-python-final/dataY.csv')
-
-dataX = csv.reader(dx)
+dataX = csv.reader(dataX)
 dataY = csv.reader(dataY)
-
-for rows in dx:
-    X.append(rows)
+X = []
+for rows in dataX:
+     X.append(len(rows[0])>0)
+for rows in dataY:
+     X.append(len(rows[0])>0)
 print(X)
+
+the.plot (dataX) 
