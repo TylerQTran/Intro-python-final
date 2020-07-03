310b88da61f845e8c010b51f13258c93886d2b9e
TylerQTran <67287706+TylerQTran@users.noreply.github.com>
2020-07-03 10:37:18 -0700

    Update intro_final.py

--- intro_final.py
+++ intro_final.py
@@ -5,9 +5,6 @@ dataX = csv.reader(dataX)
 dataY = csv.reader(dataY)
 X = []
 for rows in dataX:
-     X.append(len(rows[0])>0)
-for rows in dataY:
-     X.append(len(rows[0])>0)
+    if len(float(rows[0])>0):
+         X.append(float(rows[0]))
 print(X)
-
-the.plot (dataX) 
